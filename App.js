e4e442accc82ceda3112534ec7a7bb056e2169ce
Rafael dos Anjos <117334834+rafaelanjosjesus@users.noreply.github.com>
2023-06-02 20:55:59 -0300

    Class 6
    In this Class i learning more about flexbox and Scroll view!

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button} from 'react-native';
-import Header from './Components/Header';
-import Body from './Components/Body';
-import Footer from './Components/Footer';
-export default class App extends React.Component{ 
+import { StyleSheet, Text, View, Image, Button, ScrollView} from 'react-native';
+import Header from './Componentes/Header.js';
+import Footer from './Componentes/Footer.js';
+import Body from './Componentes/Body.js';
 
-    render(){ 
-        return(
-           <View style = {{marginTop: 20}}> 
-           <Header></Header>
-           <Body></Body>
-           <Footer></Footer>
-            </View>
-        );
-    }
+export default class App extends React.Component{
+
+  render(){
+    return (
+    <ScrollView style = {{marginTop:20, flex:1}}>
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </ScrollView>
+  );
+ }
 }
+//ScrollView / U need put this for Scroll your text or another things!
--- Components/Body.js
+++ Components/Body.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image } from 'react-native';
 
 const Body = () => {
     return (
-        <View style = {{backgroundColor: '#400c06', padding: 20}}>
-        <Text style = {{textAlign: 'center', fontSize: 30, color: 'white'}}>Body test</Text>
-        </View>
-    )
+        <View style = {{flex:1, flexDirection: 'row', flexWrap: 'wrap', backgroundColor: '#7020e8'}}>
+            <Text style ={{width:'50%', padding: 20, color: 'white', fontSize: 20}}>Diz-me com quem andas e te direi se vou contigo.. Uma coisa é certa: ninguém sai vivo dessa vida.. Toda regra tem exceção.. Diz-me com quem andas e te direi se vou contigo.. O sonho não acabou. E ainda temos pão doce, maria-mole e queijadinha..
+Na vida tudo é passageiro, menos o motorista e o cobrador…. A única mulher que andou na linha o trem pegou.. Os últimos serão os primeiros e os do meio sempre serão os do meio.. O importante é o que importa.. É hoje que eu volto amanhã..
+Mais vale um pássaro na mão do que bois voando.. Uma coisa é certa: ninguém sai vivo dessa vida.. Um é pouco, dois é bom e três é ímpar.. Agora, mais do que nunca, é a hora.. Se barba fosse respeito, bode não tinha chifre..
+Há males que vêm para o pior.. Sonhar que está comendo uma maria-mole gigante pode significar acordar sem o travesseiro!. Uma coisa é certa: ninguém sai vivo dessa vida.. Sonhar que está comendo uma maria-mole gigante pode significar acordar sem o travesseiro!. Se barba fosse respeito, bode não tinha chifre..
+Thank you for using my words in your work. Em rio de piranhas, jacaré nada de costas.. Por que 'tudo junto' se escreve separado e 'separado' se escreve tudo junto?. Essas TVs de LSD são uma droga!. Por causa da pressa, é que a mosca nasceu sem osso..
+O sol nasce, a bicicleta anda, o lobo uiva e o urso panda.. Evite acidentes. Faça de propósito.. Cemeteries are just garbage dumps filled with humans. Como se escreve o número 0 em algarismos romanos?. Se tamanho fosse documento o elefante era dono do circo..
+Daqui pra frente, quanto mais, principalmente.. Daqui pra frente, quanto mais, principalmente.. Antes tarde do que mais tarde.. Há muitas coisas mais importantes que o dinheiro...mas elas custam caro!. Na vida tudo é passageiro, menos o motorista e o cobrador….
+Um é pouco, dois é bom e três é ímpar.. O dinheiro não traz felicidade, então, dê todo o seu pra mim e seja feliz.. A única mulher que andou na linha o trem pegou.. Evite acidentes. Faça de propósito.. O dinheiro não traz felicidade, então, dê todo o seu pra mim e seja feliz..
+É hoje que eu volto amanhã.. A única mulher que andou na linha o trem pegou.. Não concordo nem discordo, muito pelo contrário.. Sonhar que está comendo uma maria-mole gigante pode significar acordar sem o travesseiro!. Jesus salva! Que passa para Moisés, que chuta e é gooooolllll....
+Jesus salva! Que passa para Moisés, que chuta e é gooooolllll.... O sol nasce, a bicicleta anda, o lobo uiva e o urso panda.. You say potatoe, I say starchy carbs. O importante é o que importa.. I have a moral code, but I haven't figured out how to read it yet. </Text>
+        <Image
+        style = {{width:200, height: 200,}}
+        source = {{
+            uri: 'https://avatars.githubusercontent.com/u/117334834?v=4'
+        }}></Image>
+              </View>
+    );
 }
-
-export default Body
+export default Body;
+//FontSize: Font Size é o tamanho do texto!
+//Width: Tamanho do objeto!
+//color: é a cor do seu texto!
